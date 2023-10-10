61514cf1ec37485ac2eda5d5c4f8b26faa1b0e79
cattynip <hi@cattynip.dev>
2023-10-09 20:11:20 -0400

    #6.2 BottomNavigationBar

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,10 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class MainNavigationScreen extends StatelessWidget {
+class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
 
   @override
+  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
+}
+
+class _MainNavigationScreenState extends State<MainNavigationScreen> {
+  int _selectedIdx = 0;
+
+  final List<Widget> screens = [
+    const Center(
+      child: Text("Home"),
+    ),
+    const Center(
+      child: Text("Search"),
+    ),
+    const Center(
+      child: Text("Settings"),
+    ),
+  ];
+
+  void _onBottomNavigationBarItemTapped(int index) {
+    setState(() {
+      _selectedIdx = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: screens[_selectedIdx],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIdx,
+        onTap: _onBottomNavigationBarItemTapped,
+        selectedItemColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.black.withOpacity(0.5),
+        type: BottomNavigationBarType.shifting, // ...wow
+        items: const [
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.house),
+            label: "Home",
+            tooltip: "Wanna go home?",
+            backgroundColor: Colors.amber,
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+            label: "Search",
+            backgroundColor: Colors.blue,
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.gear),
+            label: "Settings",
+            backgroundColor: Colors.lime,
+          ),
+        ],
+      ),
+    );
   }
 }
