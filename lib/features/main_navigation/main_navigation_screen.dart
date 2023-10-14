932b20ed957dbb46cbcdd2466fe9b902290a97bb
cattynip <hi@cattynip.dev>
2023-10-13 22:27:59 -0400

    #6.7 Stateful Navigation part Two

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -18,24 +18,6 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
   // However, if you add that property, they will be disposed and re-rendered every time the user moves between the pages.
   // Which means they will no longer save the data when the user escapes from that page because that page will be disposed and the data in that page will also be disposed.:wq
 
-  final List<Widget> screens = [
-    StfScreen(
-      key: GlobalKey(),
-    ),
-    StfScreen(
-      key: GlobalKey(),
-    ),
-    StfScreen(
-      key: GlobalKey(),
-    ),
-    StfScreen(
-      key: GlobalKey(),
-    ),
-    StfScreen(
-      key: GlobalKey(),
-    ),
-  ];
-
   void _onBottomNavBarItemTapped(int index) {
     setState(() {
       _selectedIdx = index;
@@ -45,7 +27,26 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: screens.elementAt(_selectedIdx),
+      body: Stack(
+        children: [
+          Offstage(
+            offstage: _selectedIdx != 0,
+            child: const StfScreen(),
+          ),
+          Offstage(
+            offstage: _selectedIdx != 1,
+            child: const StfScreen(),
+          ),
+          Offstage(
+            offstage: _selectedIdx != 3,
+            child: const StfScreen(),
+          ),
+          Offstage(
+            offstage: _selectedIdx != 4,
+            child: const StfScreen(),
+          ),
+        ],
+      ),
       bottomNavigationBar: BottomAppBar(
         color: Colors.black,
         child: Row(
