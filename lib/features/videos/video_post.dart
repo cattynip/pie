e3c65992ba1a74bd11de115a88e7753840e85585
cattynip <hi@cattynip.dev>
2023-10-18 21:49:36 -0400

    package: install a package

--- lib/features/videos/video_post.dart
+++ lib/features/videos/video_post.dart
@@ -10,6 +10,6 @@ class VideoPost extends StatefulWidget {
 class _VideoPostState extends State<VideoPost> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Stack;
   }
 }
